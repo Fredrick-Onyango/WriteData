8008c64cb18927950ac735163ff52ddcd10b01c5
Fredrick Onyango <fonyango@livinggoods.org>
2019-07-18 09:09:57 +0300

    New Class

---
+++ https:/github.com/Fredrick-Onyango/WriteData.git/WriteData/src/app/newClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package app;
+
+/**
+ * @author fredrickonyango
+ *
+ */
+public class newClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Test");
+	}
+
+}
